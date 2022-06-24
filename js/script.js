f5ecd0e8b083d3a013b4e0db84f05c43e75c259d
Abai <example@gmail.com>
2022-06-24 23:34:09 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18", "");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("Whats your name", '');
+answers[1] = prompt("Whats your surname", '');
+answers[2] = prompt("How old are you", '');
+
+document.write(answers);
