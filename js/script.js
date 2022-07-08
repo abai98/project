9be7dff0ad6a37c602d134d27606f6fa0b85b571
Abai <example@gmail.com>
2022-07-08 14:15:34 +0600

    numberOfFilms example MANIZDI

--- js/script.js
+++ js/script.js
@@ -1,75 +1,19 @@
 "use strict";
 
-const findSmallMassive = [5, 355, 685, 2];
-let max = findSmallMassive[0];
-
-for (let i = 0; i < findSmallMassive.length; i++) {
-    if (findSmallMassive[i] > max) {
-        max = findSmallMassive[i];
-        console.log(max);
-    }
-}
-
-
-
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
-    }
+const numberofFilms = +prompt("How much film you watched", ''); // плюс означает то что будет равно числу
+const personalMovieDB =  {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const family = [];
+const oneOfFilms = prompt("Один из последних просмотренных фильмов?", ''),
+      howMuch = prompt("На сколько оцените его?", ''),
+      twoOfFilms = prompt("Один из последних просмотренных фильмов?", ''),
+      twohowMuch = prompt("На сколько оцените его?", '');
 
-function showFamily(arr) {
-    if (arr != '') {
-        console.log(`Семья состоит из ${arr}`);
-    } else {
-        console.log("Семья пуста");
-    }
-}
-
-showFamily(family);
-
-
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin','DCDSVFDS'];
-
-function standardizeStrings(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let a = arr[i].toLowerCase();
-        console.log(a);
-    }
-}
-
-standardizeStrings(favoriteCities);
-
-
-
-const someString = 'This is some strange string';
-
-function reverse(str) {
-    let a = str.reverse();
-    console.log(a);
-}
-
-reverse(someString);
-
-
-
-
-let str1 = "Cause i love you";
-
-let j = [];
-
-j = str1;
-
-console.log(j);
-
+personalMovieDB.movies[oneOfFilms] = howMuch;
+personalMovieDB.movies[twoOfFilms] = twohowMuch;
 
